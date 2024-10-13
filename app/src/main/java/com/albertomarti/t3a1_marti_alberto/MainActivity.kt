7479e76467694714089fb391da804181c8e25233
Almartar <alberto.marti@hotmail.com>
2024-10-13 21:05:10 +0200

    tarea3act1

---
+++ app/src/main/java/com/albertomarti/t3a1_marti_alberto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.albertomarti.t3a1_marti_alberto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "t3a1-marti-alberto"
+include(":app")
+ 
